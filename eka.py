dd26970bdb998493d0382663168db55387b51d16
ekaaaaristia <angeliapoluakan@gmail.com>
2025-11-17 21:02:20 +0800

    Update eka.py

--- eka.py
+++ eka.py
@@ -1,1 +1,18 @@
-
+def inputbil():
+    bilangan = int(input("masukkan bilangan: "))
+    return bilangan
+def idr_eur():
+    konv = bil//19.348
+    print(f"{konv:,.0f}")
+def eur_idr():
+    konve = bil*19.348
+    print(f"{konve:,.3f}")
+    
+print("1. IDR - EUR")
+print("2. EUR - IDR")
+pil = input("masukkan pilihan anda: ")
+bil = inputbil()
+if pil=="1":
+    idr_eur()
+elif pil=="2":
+    eur_idr()
